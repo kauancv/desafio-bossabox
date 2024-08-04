1b763d480646fb1612af68d2ff2fd5f2d797e9ae
kauan-aspire <kauancv239@gmail.com>
2024-08-04 17:28:20 -0300

    Criado o projeto com o Spring Initializr

---
+++ src/test/java/com/bossabox/desafio/DesafioBossaboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bossabox.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBossaboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
